fea5018c04db52571e5474f52ba0f425a3bae178
Nahum <nicolasdrv10@gmail.com>
2025-02-18 10:46:15 -0500

    Cree la clase Note y le agregue las constantes high medium y low

--- app/notebook.py
+++ app/notebook.py
@@ -1,1 +1,10 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+from datetime import datetime
+
+
+
+class Note:
+    HIGH: str = "HIGH"
+    MEDIUM: str = "MEDIUM"
+    LOW: str = "LOW"
+
