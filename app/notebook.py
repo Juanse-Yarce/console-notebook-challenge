1a2a32e6d78cd0dd05687c34a554be85ae7afc1b
Juan Sebastian Lopez <jlopezyarce@gmail.com>
2025-02-20 11:35:17 -0500

    cree el metodo important_notes

--- app/notebook.py
+++ app/notebook.py
@@ -42,12 +42,12 @@ class Notebook:
                 self.notes.remove(note)
                 break
 
+    def important_notes(selfself) -> list[Note]:
+        resulting_notes: list[Note] = []
+        for note in self.notes:
+            if note.importance == Note.HIGH or Note.MEDIUM:
+                resulting_notes.append(note)
+        return resulting_notes
 
-def important_notes(selfself) -> list[Note]:
-    resulting_notes: list[Note] = []
-    for note in self.notes:
-        if note.importance == Note.HIGH or Note.MEDIUM:
-            resulting_notes.append(note)
-    return resulting_notes
 
 
